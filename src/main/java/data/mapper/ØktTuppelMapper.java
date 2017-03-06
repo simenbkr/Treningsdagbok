b727922855014a94ab85748900425e3c9cae03f7
TommyWithATiger <halvard.hummel@hotmail.com>
2017-03-06 15:11:22 +0100

    Fixed the ØktTuppelMapper

--- src/main/java/data/mapper/ØktTuppelMapper.java
+++ src/main/java/data/mapper/ØktTuppelMapper.java
@@ -1,6 +1,10 @@
 package data.mapper;
 
-import data.models.ØktTuppel;
+import data.dao.MiljøDAO;
+import data.dao.ResultatDAO;
+import data.dao.ØktDAO;
+import data.dao.ØvelseDAO;
+import data.models.*;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
@@ -8,6 +12,10 @@ import java.sql.SQLException;
 
 public class ØktTuppelMapper implements RowMapper<ØktTuppel> {
     public ØktTuppel mapRow(ResultSet resultSet, int i) throws SQLException {
-        return null;
+        Økt økt = new ØktDAO().getByID(resultSet.getInt("Økt_id"));
+        Øvelse øvelse = new ØvelseDAO().getByID(resultSet.getInt("Øvelse_id"));
+        Miljø miljø = new MiljøDAO().getByID(resultSet.getInt("Miljø_id"));
+        Resultat resultat = new ResultatDAO().getByID(resultSet.getInt("Resultat_id"));
+        return new ØktTuppel(økt, øvelse, miljø, resultat);
     }
 }
