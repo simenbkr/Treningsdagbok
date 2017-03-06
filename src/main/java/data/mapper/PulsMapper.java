6ee065b78fd46285a58d7ba7b4ab6502e561f7a7
TommyWithATiger <halvard.hummel@hotmail.com>
2017-03-06 12:04:16 +0100

    Added a mapper for Puls

---
+++ src/main/java/data/mapper/PulsMapper.java
@@ -0,0 +1,14 @@
+package data.mapper;
+
+import data.models.Puls;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PulsMapper implements RowMapper<Puls> {
+    public Puls mapRow(ResultSet resultSet, int i) throws SQLException {
+        return new Puls(resultSet.getInt("id"), resultSet.getTimestamp("tid"), resultSet.getInt("puls"),
+                resultSet.getDouble("lengde"), resultSet.getDouble("høyde"), resultSet.getDouble("bredde"), resultSet.getInt("øktId"));
+    }
+}
