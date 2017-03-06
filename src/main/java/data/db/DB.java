28450aa9b8120d75dbdd2fc4f044a6c9af7751b4
simenbkr <simen@haitech.org>
2017-03-06 11:56:39 +0100

    La til sånn databasetilkobler jo.

---
+++ src/main/java/data/db/DB.java
@@ -0,0 +1,45 @@
+package data.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DB {
+
+    private static DB instance = new DB();
+    private static final String URL = "jdbc:mysql://localhost/TreningsDagbok";
+    private static final String USER = "kakemann";
+    private static final String PASSWORD = "jeghaterbarnmedraraksent";
+    private static final String DRIVER_CLASS = "com.mysql.jdbc.Driver";
+
+    private DB() {
+        try {
+            Class.forName(DRIVER_CLASS);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Connection lagKobling() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+
+            System.out.println("ERROR: Unable to Connect to Database.");
+        }
+        return connection;
+    }
+
+    public static Connection getConnection() {
+        return instance.lagKobling();
+    }
+
+    public static void main(String[] args){
+        //DB db = new DB();
+        Connection kobling = DB.getConnection();
+    }
+
+}
