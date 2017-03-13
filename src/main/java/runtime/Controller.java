e94cbeb29aeb5c135ecda701e3367dc9340054be
TommyWithATiger <halvard.hummel@hotmail.com>
2017-03-13 11:22:12 +0100

    Formatting

--- src/main/java/runtime/Controller.java
+++ src/main/java/runtime/Controller.java
@@ -1,6 +1,7 @@
 package runtime;
 
 
+import runtime.handler.BestHandler;
 import runtime.handler.IHandler;
 import runtime.handler.statistics.StatisticsHandler;
 
@@ -12,12 +13,19 @@ import java.util.Scanner;
 public class Controller {
 
     private List<IHandler> handlers = new ArrayList<IHandler>(Arrays.asList(new IHandler[]{
-            new StatisticsHandler()
+            new StatisticsHandler(),
+            new BestHandler()
     }));
 
     public void run() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
+            System.out.println("Kommandoer:");
+            System.out.println("------------");
+            System.out.println("statistikk - Viser statistikk for siste måned");
+            System.out.println("beste - Viser beste økter");
+            System.out.println("");
+            System.out.print("> ");
             String input = scanner.nextLine();
             for (IHandler handler : handlers) {
                 if (handler.validCommand(input)) handler.handle(scanner);
